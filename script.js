7d2cd701d6d11a634ecbf280d6d9ef02acf9702d
BlackSpadesZ715 <boringuser0123@gmail.com>
2026-05-10 13:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,34 @@ window.addEventListener("scroll", () => {
   const moon = document.getElementById("moon");
   moon.style.top = `${80 - progress * 60}%`;
 });
+const flowerContainer = document.querySelector(".flowers");
+
+function createPetal() {
+  const petal = document.createElement("div");
+  petal.classList.add("petal");
+
+  petal.style.left = Math.random() * 100 + "vw";
+  petal.style.animationDuration = 5 + Math.random() * 5 + "s";
+
+  flowerContainer.appendChild(petal);
+
+  setTimeout(() => {
+    petal.remove();
+  }, 10000);
+}
+
+setInterval(createPetal, 300);
+document.addEventListener("mousemove", (e) => {
+  const spark = document.createElement("div");
+  spark.classList.add("spark");
+
+  spark.style.left = e.pageX + "px";
+  spark.style.top = e.pageY + "px";
+
+  document.body.appendChild(spark);
+
+  setTimeout(() => {
+    spark.remove();
+  }, 500);
+});
+
